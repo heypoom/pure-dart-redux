56ac0aeca263c8d3c8ae0cd69aca54a165831863
Phoomparin Mano <work@phoom.in.th>
2018-09-16 02:39:08 +0700

    :sparkles: Added TODO List Example

--- main.dart
+++ main.dart
@@ -1,13 +1,55 @@
+class Todo {
+  final String name;
+
+  Todo(this.name);
+}
+
 // -- State: บอกว่าเก็บข้อมูลอะไรบ้าง --
+
+class State {
+  final List<Todo> todos;
+
+  State({this.todos = const []});
+}
 
 // -- Action: บอกว่าจะทำอะไร --
 
+abstract class TodoAction {}
+
+class AddTodo extends TodoAction {
+  final String name;
+
+  AddTodo(this.name);
+}
+
 // -- Reducer: บอกว่าจะเปลี่ยนแปลงข้อมูลอย่างไร --
 
+List<Todo> TodoReducer(List<Todo> state, TodoAction action) {
+  if (action is AddTodo) {
+    return <Todo>[]
+      ..addAll(state)
+      ..add(Todo(action.name));
+  }
+
+  return state;
+}
+
 // -- Root Reducer: รวบ Reducer ทุกตัว แล้วเอามาสร้าง State ใหม่
+
+State rootReducer(State state, dynamic action) {
+  return State(todos: TodoReducer(state.todos, action));
+}
 
 // ลองรันดูกัน!
 
 void main() {
-  print('Hello!');
+  final state = State();
+  final action = AddTodo("Buy Milk");
+
+  final newState = TodoReducer(state.todos, action);
+
+  final moreAction = AddTodo("Buy Socks");
+  final moreState = TodoReducer(newState, moreAction);
+
+  print("State is ${moreState.map((x) => x.name)}");
 }
