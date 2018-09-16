f39ba8cf8d8b99b4f026d4b1a771ff8df5ef9760
Phoomparin Mano <work@phoom.in.th>
2018-09-16 10:17:05 +0700

    :sparkles: Added User List Example

--- main.dart
+++ main.dart
@@ -1,13 +1,54 @@
+// -- Data Type --
+
+class User {
+  final String name;
+  final int salary;
+
+  User([this.name = '☁️', this.salary = 9000]);
+}
+
 // -- State: บอกว่าเก็บข้อมูลอะไรบ้าง --
+class State {
+  final List<User> users;
+
+  State({this.users = const []});
+}
 
 // -- Action: บอกว่าจะทำอะไร --
 
+abstract class UserAction {}
+
+class AddUser extends UserAction {
+  final String name;
+  final int salary;
+
+  AddUser([this.name, this.salary]);
+}
+
 // -- Reducer: บอกว่าจะเปลี่ยนแปลงข้อมูลอย่างไร --
 
+List<User> userReducer(List<User> state, UserAction action) {
+  if (action is AddUser) {
+    return <User>[]
+      ..addAll(state)
+      ..add(User(action.name, action.salary));
+  }
+
+  return state;
+}
+
 // -- Root Reducer: รวบ Reducer ทุกตัว แล้วเอามาสร้าง State ใหม่
+
+State rootReducer(State state, dynamic action) {
+  return State(users: userReducer(state.users, action));
+}
 
 // ลองรันดูกัน!
 
 void main() {
-  print('Hello!');
+  final state = State();
+  final action = AddUser('🐘');
+
+  final newState = rootReducer(state, action);
+  print("New State: ${newState.users.map((x) => x.name)}");
 }
